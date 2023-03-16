a3e0a6cbcffd0478c57b2e2edfd48116e1400a34
Kioko422 <oliverkioko12@gmail.com>
2023-03-16 15:29:01 -0700

    6-size.c commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Code entry
+ * Description: C program to print the size of data types.
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+P
