5c65226c42696da2032e3d49cb0e2a4306bc8ff5
Kioko422 <oliverkioko12@gmail.com>
2023-03-17 14:10:07 -0700

    5-print_numbers.c commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Code entry
+ * Description: Print digits of base 10 from 0
+ * Return:0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
