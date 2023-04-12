1c647fa2a2bbf3537da720bad6af42b4ec4d6895
Oliver Kioko <oliverkioko12@gmail.com>
2023-04-12 01:20:37 -0700

    Write a function that returns the natural square root of a number commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,44 +1,43 @@
-#include "main.h"
+#include "holberton.h"
 
-int sqrts(int n, int i);
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
 
 /**
- * _sqrt_recursion - natural squareroot of a number
- * @n: The number
- * Return: -1 if n doesn't have a natural squareroot
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
+ *
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
  */
-
-int _sqrt_recursion(int n)
+int find_sqrt(int num, int root)
 {
-	int a = 0;
+	if ((root * root) == num)
+		return (root);
 
-	if (n < 0)
-	{
+	if (root == num / 2)
 		return (-1);
-	}
-	if (n == 1)
-	{
-		return (1);
-	}
-	return (sqrts(n, a));
+
+	return (find_sqrt(num, root + 1));
 }
 
 /**
- * sqrts - calculates square root of number
- * @i: number
- * @j: square
- * Return: square root
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square root of.
+ *
+ * Return: If n has a natural square root - the natural square root of n.
+ *         If n does not have a natural square root - -1.
  */
-
-int sqrts(int i, int j)
+int _sqrt_recursion(int n)
 {
-	if ((i * i) == j)
-	{
-		return (i);
-	}
-	if (i == (j / 2))
-	{
+	int root = 0;
+
+	if (n < 0)
 		return (-1);
-	}
-	return (sqrts(j, i + 1));
+
+	if (n == 1)
+		return (1);
+
+	return (find_sqrt(n, root));
 }
