b6c28bd6a987730ab785256e06afbe17cbf4fdbc
Kioko <oliverkioko12@gmail.com>
2023-05-03 06:00:36 -0700

    singly linked lists commit

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,22 @@
-8#include "lists.h"
+#include "lists.h"
+#include <stdio.h>
 
 /**
- * listint_len - Returns number of elements in a linked list
- * @h: elements of linked list
- * Return: Number of elements
+ * listint_len - Returns the number of elements
+ *               in a linked listint_t list.
+ * @h: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of elements in the listint_t list.
  */
-
 size_t listint_len(const listint_t *h)
 {
-	size_t nodes_number = 0;
+	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		nodes_number++;
+		nodes++;
 		h = h->next;
 	}
-	return (nodes_number);
+
+	return (nodes);
 }
